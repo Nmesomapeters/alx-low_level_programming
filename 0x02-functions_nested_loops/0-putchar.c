5ba591b0cdd84d61197cb795378d92b91a892983
Nmesoma <nmesomaokeke9@gmail.com>
2023-04-26 10:26:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "Nmesomapeters"
+
+/**
+ * main - prints the string "Nmesomapeters" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "Nmesomapeters";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
