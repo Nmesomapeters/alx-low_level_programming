19bbebe609ebca96b37d8d9564a7b9b9c293b885
Nmesoma <nmesomaokeke9@gmail.com>
2023-05-24 17:00:36 -0700

    my function_pointers.h file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,14 +1,22 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
-
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * File: main
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Desc: header file
-**/
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);/*prototype _putchar*/
+
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c);
+/*prototype function that prints a name.*/
+
 void array_iterator(int *array, size_t size, void (*action)(int));
+/*prototype function that executes a function*/
+
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
