9c09e61b71a81d4566eba29e7356a46a8ac76abd
Nmesomapeters <105252598+Nmesomapeters@users.noreply.github.com>
2023-06-09 01:15:13 +0000

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,18 +4,15 @@
  * get_bit -  function that gets a bit at the index.
  * @n: number
  * @index: index
- * Return:value of the bit or -1 only if an error occured.
+ * Return: value of the bit 1 or -1 only if an error occured.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int maximum = 0x01;
-
-	maximum <<= index;
-	if (maximum == 0)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	if ((n & maximum))
-		return (1);
-	else
+	if ((n & (1 << index)) == 0)
 		return (0);
+
+	return (1);
 }
